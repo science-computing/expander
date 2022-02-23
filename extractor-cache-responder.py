0ae0e8412dd3e013528b35a6a2d15c1670ec3f5b
Michael Weiser <michael.weiser@gmx.de>
2022-02-23 15:34:52 +0000

    cache-responder: Add aging-out of cache
    Keeping cached results indefinitely not only uses up storage but
    prevents them from fresh analysis. Add a simple method of aging-out old
    cache entries to test both properties.

--- extractor-cache-responder.py
+++ extractor-cache-responder.py
@@ -1,6 +1,6 @@
 #!/home/michael/karton-venv/bin/python3
 
-#import datetime
+import datetime
 import hashlib
 import json
 import sys
@@ -11,6 +11,9 @@ USE_DEDUPER = True
 
 EXTRACTOR_REPORTS = "extractor.reports"
 EXTRACTOR_JOB_CACHE = "extractor.cache:"
+
+CACHE_AGE_OUT_INTERVAL = datetime.timedelta(seconds=60)
+CACHE_MAX_AGE = datetime.timedelta(seconds=240)
 
 config = karton.core.Config(sys.argv[1])
 
@@ -36,7 +39,37 @@ class PeekabooCacheResponder(karton.core.Karton):
 
         super().__init__(config=config, identity=identity, backend=backend)
 
+        # do gc on first task
+        self.last_age_out = datetime.datetime(
+            1970, 1, 1, tzinfo=datetime.timezone.utc)
+
+        # come to think of it, do gc now
+        self.age_out()
+
+    def age_out(self):
+        now = datetime.datetime.now(datetime.timezone.utc)
+        if self.last_age_out + CACHE_AGE_OUT_INTERVAL > now:
+            return
+
+        cutoff_date = datetime.datetime.now(
+            datetime.timezone.utc) - CACHE_MAX_AGE
+        cutoff_score = cutoff_date.timestamp() / 3600
+        for entry in self.backend.redis.keys(EXTRACTOR_JOB_CACHE + "*"):
+            # keys becoming empty makes them magically disappear as well
+            aged_out = self.backend.redis.zremrangebyscore(
+                entry, 0, cutoff_score)
+            if aged_out:
+                self.log.info(
+                    "Removed %d old entries from cache entry %s",
+                    aged_out, entry)
+
+        self.last_age_out = now
+
     def process(self, task: karton.core.Task) -> None:
+        # aging out old cache entries not only saves space but causes
+        # duplicated, fresh analysis after cache became empty - may be desired
+        self.age_out()
+
         sample = task.get_resource("sample")
 
         # file name can be None in resource
@@ -82,7 +115,8 @@ class PeekabooCacheResponder(karton.core.Karton):
 
             # optionally add to cache itself - does that make sense?
             # that makes it pretend to be the most recent report although it's
-            # a copy of a cached one
+            # a copy of a cached one - would prevent aging-out (see above),
+            # i.e. keep frequently-used cache entries "hot"
             #now = datetime.datetime.now(
             #    datetime.timezone.utc).timestamp() / 3600
             #self.backend.redis.zadd(cache_key, {task.root_uid: now})
