36489a56fb860831796798974982884e2b523e97
Michael Weiser <michael.weiser@gmx.de>
2022-02-23 15:16:52 +0000

    deduper: Collect garbage
    Add a simple garbage collector that tries hard to detect and clear all
    garbage from redis.

--- extractor-deduper.py
+++ extractor-deduper.py
@@ -25,6 +25,8 @@ DEDUPE_RECHECK = 2
 # give up deduping after ten minutes
 DEDUPE_CUTOFF = datetime.timedelta(seconds=60)
 
+DEDUPE_GC_INTERVAL = 2 * DEDUPE_CUTOFF
+
 config = karton.core.Config(sys.argv[1])
 
 
@@ -44,7 +46,84 @@ class ExtractorDeduper(common.DelayingKarton):
         }
     ]
 
+    def __init__(self, config=None, identity=None, backend=None, timeout=1):
+        super().__init__(
+            config=config, identity=identity, backend=backend, timeout=timeout)
+
+        # do gc on first task
+        self.last_gc = datetime.datetime(
+            1970, 1, 1, tzinfo=datetime.timezone.utc)
+        self.orphaned_locks = []
+
+        # come to think of it, do gc now
+        self.collect_garbage()
+
+    def collect_garbage(self):
+        now = datetime.datetime.now(datetime.timezone.utc)
+        if self.last_gc + DEDUPE_GC_INTERVAL > now:
+            return
+
+        running = self.backend.redis.hgetall(EXTRACTOR_RUNNING)
+        running_reverse = {}
+        for criteria, job_id in running.items():
+            running_reverse[job_id] = criteria
+
+        running_since = self.backend.redis.hgetall(EXTRACTOR_RUNNING_SINCE)
+        for job_id, timestamp in running_since.copy().items():
+            # twice the cutoff makes them doubly invalid - they've been ignored
+            # since the cutoff was reached
+            since = datetime.datetime.fromisoformat(timestamp)
+            if since + 2 * DEDUPE_CUTOFF < now:
+                # backreference into running locks to keep them consistent
+                criteria = running_reverse.get(job_id)
+                if criteria is not None:
+                    self.backend.redis.hdel(EXTRACTOR_RUNNING, criteria)
+                    del running_reverse[job_id]
+                    del running[criteria]
+
+                self.log.info("Clearing old timestamp of job %s", job_id)
+                self.backend.redis.hdel(EXTRACTOR_RUNNING_SINCE, job_id)
+                del running_since[job_id]
+
+        for criteria, job_id in running.copy().items():
+            if not self.backend.redis.hexists(EXTRACTOR_REPORTS, job_id):
+                continue
+
+            self.log.info(
+                "Clearing lock %s of finished job %s", criteria, job_id)
+            self.backend.redis.hdel(EXTRACTOR_RUNNING, criteria)
+            del running[criteria]
+
+            # forward reference into timestamps to keep consistency
+            if running_since.get(job_id) is not None:
+                self.backend.redis.hdel(EXTRACTOR_RUNNING_SINCE, job_id)
+                del running_since[job_id]
+
+        # clear out locks which still have no timestamp assigned in this
+        # iteration
+        for criteria in self.orphaned_locks:
+            job_id = self.backend.redis.hget(EXTRACTOR_RUNNING, criteria)
+            if job_id is None:
+                continue
+
+            if self.backend.redis.hexists(EXTRACTOR_RUNNING_SINCE, job_id):
+                continue
+
+            self.log.info("Clearing orphaned lock %s", criteria)
+            self.backend.redis.hdel(EXTRACTOR_RUNNING, criteria)
+
+        self.orphaned_locks = []
+        for criteria, job_id in running.items():
+            if running_since.get(job_id) is not None:
+                continue
+
+            self.orphaned_locks.append(criteria)
+
+        self.last_gc = now
+
     def process(self, task: karton.core.Task) -> None:
+        self.collect_garbage()
+
         deduped_headers = {
             "type": "sample",
             "kind": "raw",
@@ -101,9 +180,6 @@ class ExtractorDeduper(common.DelayingKarton):
                 since = now.isoformat()
                 self.backend.redis.hset(
                     EXTRACTOR_RUNNING_SINCE, task.root_uid, since)
-
-                # FIXME: We're accumulating two hash keys here which need
-                # garbage-collecting.
 
                 # pass on
                 task = task.derive_task(deduped_headers)
