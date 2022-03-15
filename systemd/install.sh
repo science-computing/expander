8d46ae99e200645cb4fda513aa99aa2b6352a41c
Michael Weiser <michael.weiser@gmx.de>
2022-03-15 10:05:55 +0000

    systemd: Add minimal systemd config
    Add systemd unit files for the whole system including minio. Try to keep
    them redunancy-free by using common drop-in config files with
    service-specific overrides as required. A minimal installer shell script
    is added for documentation and as starting point for actual installers.
    
    Unfortunately, minio has no real distribution infrastructure (i.e.
    automatically downloading and installing the latest release package)
    apart from container images. For now we use a moving-target deb
    package release download URL.
    
    The dashboard is added for convenience. It requires separate
    configuration. We add respective environment variables for simplicity.

---
+++ systemd/install.sh
@@ -0,0 +1,42 @@
+#!/bin/bash
+
+apt-get update redis
+apt-get install redis
+
+virtualenv /opt/extractor
+/opt/extractor/bin/pip install ..
+/opt/extractor/bin/pip install karton-dashboard karton-classifier karton-archive-extractor
+/opt/extractor/bin/pip install itsdangerous==2.0.1
+
+mkdir /opt/extractor/etc
+cp ../extractor.ini /opt/extractor/etc/
+useradd -m extractor
+
+wget https://dl.min.io/server/minio/release/linux-amd64/minio_20220314182524.0.0_amd64.deb -O minio.deb
+dpkg -i minio.deb
+rm minio.deb
+
+cp default/minio /etc/default
+cp -r *.service* /etc/systemd/system
+
+# brace expansion is a bashism
+for i in karton-{system,logger,classifier,archive-extractor} ; do
+	echo -e "[Install]\nWantedBy=multi-user.target" > /etc/systemd/system/$i@.service
+	systemctl enable $i@{1,2,3}
+	systemctl start $i@{1,2,3}
+done
+
+cp karton-dashboard.service /etc/systemd/system/karton-dashboard.service
+systemctl enable karton-dashboard
+systemctl start karton-dashboard
+
+for i in extractor-{deduper,cache-responder,peekaboo-submitter,poker,peekaboo-tracker,correlator} ; do
+	echo -e "[Install]\nWantedBy=multi-user.target" > /etc/systemd/system/$i@.service
+
+	systemctl enable $i@{1,2,3}
+	systemctl start $i@{1,2,3}
+done
+
+cp extractor-api.service /etc/systemd/system/extractor-api.service
+systemctl enable extractor-api
+systemctl start extractor-api
